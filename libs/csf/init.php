e8be4affc49434ab79a4539ab15388331bab6c63
Orhan Osmanoglu <devorhancom@gmail.com>
2023-09-04 00:00:18 +0300

    edit csf

--- libs/csf/init.php
+++ libs/csf/init.php
@@ -5,3 +5,7 @@ if (!defined('ABSPATH')) {
 }
 
 require_once _DO_DIR . '/libs/csf/codestar-framework.php';
+
+/* --------------------------------- Options -------------------------------- */
+// Admin page options
+require_once _DO_DIR . '/libs/csf/options/admin.php';
